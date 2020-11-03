325222bef45d90c781ae012b958b10a3e615b940
E.DISCORS <e.discors.perso@gmail.com>
2020-11-03 14:53:08 +0100

    remove from backup-systems

---
+++ run.py
@@ -0,0 +1,9 @@
+"""
+* This python script is used for mysql database backup
+* using mysqldump and gzip utility.
+* First version written by: Rahul Kumar
+* Written by: Emmanuel DISCORS
+"""
+
+# call check_dbbackup() module
+from dbbackup import check_dbbackup
